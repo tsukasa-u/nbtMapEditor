99e8d81f5db4930bc7559666b2a62ce590e10ddd
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2023-02-09 12:49:11 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -649,7 +649,7 @@ document.getElementById("writer").onclick = () => {
             if (ele.classList.contains("select_on")) {
                 let yx = ele.dataset.number.split("-");
                 raw_src_data = ctx.getImageData(Number(yx[1])*128, Number(yx[0])*128, 128, 128);
-                // console.log(raw_src_data);
+                console.log(raw_src_data);
 
                 let input = new Uint8Array(raw_src_data.data.length);
                 input.set(raw_src_data.data, 0);
