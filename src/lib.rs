a81c6274df5c27538a0460ded3bae6e30cdc495c
tsukasa-u <68815462+tsukasa-u@users.noreply.github.com>
2023-03-08 18:09:04 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 
 extern crate console_error_panic_hook;
-use std::panic;
+use std::{panic};
 
 use wasm_bindgen::prelude::*;
 use web_sys::console;
@@ -16,10 +16,10 @@ use wasm_bindgen::prelude::*;
 // static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 
-#[wasm_bindgen]
-pub fn test(num: i32) -> i32 {
-  num + 1i32
-}
+// #[wasm_bindgen]
+// pub fn test(num: i32) -> i32 {
+//   num + 1i32
+// }
 
 #[wasm_bindgen]
 pub fn rgb2lab(rgb: &[i32], lab: &mut [f32]) {
@@ -102,53 +102,97 @@ fn cie2000(labch1: &[f32], labch2: &[f32], kl: f32, kc: f32, kh: f32) -> f32 {
   return ((deltal/kl/sl).powi(2) + (deltacp/kc/sc).powi(2) + (deltaH/kh/sh).powi(2) + rt*deltacp/kc/sc*deltaH/kh/sh).sqrt();
 }
 
-#[wasm_bindgen]
-pub fn search_color_id(src: &[i32], output: &mut [u8], color_map: &[i32], bg: &[i32], out_image: &mut [u8]) {
-  panic::set_hook(Box::new(console_error_panic_hook::hook));
-
-  let mut min:f32 = 1000000f32;
-  let mut id:i32 = 0;
-  let mut lab1: [f32; 4] = [0f32; 4];
-  let mut lab2: [f32; 4] = [0f32; 4];
-  // let js: JsValue = output.len().into();
-  // console::log_2(&"Logging : output.len".into(), &js);
-  for n in 0..output.len() {
-    id = 0i32;
-    min = 100f32;
-    let alpha: &i32 = &src[4*n + 3];
-    let a: [i32; 3] = [
-      bg[0] + (src[4*n    ] - bg[0])*alpha/255,
-      bg[1] + (src[4*n + 1] - bg[1])*alpha/255,
-      bg[2] + (src[4*n + 2] - bg[2])*alpha/255,
-    ];
-    // let js: JsValue = output.len().into();
-    // console::log_3(&bg[0].into(), &a[0].into(), &src[4*n    ].into());
-    for _i in 0..(color_map.len()/3usize) {
-      // rgb2lab(&(src[4*n..(4*n + 3)]), &mut lab1);
-      rgb2lab(&a, &mut lab1);
-      rgb2lab(&(color_map[3*_i..(3*_i + 3)]), &mut lab2);
-      // let d: f32 = cie2000(&lab1, &lab2, 1.835f32, 1f32, 1f32);
-      let d: f32 = cie2000(&lab1, &lab2, 1f32, 1f32, 1f32);
-      if min > d {
-        min = d;
-        id = _i as i32;
-      }
-    }
-    output[n] = id as u8;
-    out_image[4*n    ] = color_map[3*(id as usize)    ] as u8;
-    out_image[4*n + 1] = color_map[3*(id as usize) + 1] as u8;
-    out_image[4*n + 2] = color_map[3*(id as usize) + 2] as u8;
-    out_image[4*n + 3] = 255u8;
-  }
-  // console::log_2(&"Logging :".into(), &1.into());
+fn rgb_d(b: &[f32], a: &[f32]) -> f32 {
+  return ((b[0]-a[0]).powi(2)+(b[1]-a[1]).powi(2)+(b[2]-a[2]).powi(2));
 }
 
+// #[wasm_bindgen]
+// pub fn search_color_id(src: &[i32], output: &mut [u8], color_map: &[i32], bg: &[i32], out_image: &mut [u8]) {
+//   panic::set_hook(Box::new(console_error_panic_hook::hook));
+
+//   let mut min:f32 = 1000000f32;
+//   let mut id:i32 = 0;
+//   let mut lab1: [f32; 4] = [0f32; 4];
+//   let mut lab2: [f32; 4] = [0f32; 4];
+//   // let js: JsValue = output.len().into();
+//   // console::log_2(&"Logging : output.len".into(), &js);
+//   for n in 0..output.len() {
+//     id = 0i32;
+//     min = 100f32;
+//     let alpha: &i32 = &src[4*n + 3];
+//     let a: [i32; 3] = [
+//       bg[0] + (src[4*n    ] - bg[0])*alpha/255,
+//       bg[1] + (src[4*n + 1] - bg[1])*alpha/255,
+//       bg[2] + (src[4*n + 2] - bg[2])*alpha/255,
+//     ];
+//     // let js: JsValue = output.len().into();
+//     // console::log_3(&bg[0].into(), &a[0].into(), &src[4*n    ].into());
+//     for _i in 0..(color_map.len()/3usize) {
+//       // rgb2lab(&(src[4*n..(4*n + 3)]), &mut lab1);
+//       rgb2lab(&a, &mut lab1);
+//       rgb2lab(&(color_map[3*_i..(3*_i + 3)]), &mut lab2);
+//       // let d: f32 = cie2000(&lab1, &lab2, 1.835f32, 1f32, 1f32);
+//       let d: f32 = cie2000(&lab1, &lab2, 1f32, 1f32, 1f32);
+//       if min > d {
+//         min = d;
+//         id = _i as i32;
+//       }
+//     }
+//     output[n] = id as u8;
+//     out_image[4*n    ] = color_map[3*(id as usize)    ] as u8;
+//     out_image[4*n + 1] = color_map[3*(id as usize) + 1] as u8;
+//     out_image[4*n + 2] = color_map[3*(id as usize) + 2] as u8;
+//     out_image[4*n + 3] = 255u8;
+//   }
+//   // console::log_2(&"Logging :".into(), &1.into());
+// }
+
+// #[wasm_bindgen]
+// pub fn search_color_id_2(src: &[i32], output: &mut [u8], color_map: &[i32], bg: &[i32], out_image: &mut [u8]) {
+//   panic::set_hook(Box::new(console_error_panic_hook::hook));
+
+//   let mut min:f32 = 100f32;
+//   let mut id:i32 = 0;
+//   let mut lab1: [f32; 4] = [0f32; 4];
+//   let mut lab2: [f32; 4] = [0f32; 4];
+//   for n in 0..output.len() {
+//     id = 100i32;
+//     min = 1000000f32;
+//     let alpha: &i32 = &src[4*n + 3];
+//     let a: [i32; 3] = [
+//       bg[0] + (src[4*n    ] - bg[0])*alpha/255,
+//       bg[1] + (src[4*n + 1] - bg[1])*alpha/255,
+//       bg[2] + (src[4*n + 2] - bg[2])*alpha/255,
+//     ];
+//     // console::log_5(&src[4*n + 3].into(), &src[4*n    ].into(), &(src[4*n    ] - bg[0]).into(), &((src[4*n    ] - bg[0])*alpha).into(), &((src[4*n    ] - bg[0])*alpha>>8).into());
+//     for _i in 0..(color_map.len()/3usize) {
+//       rgb2lab(&a, &mut lab1);
+//       rgb2lab(&(color_map[3*_i..(3*_i + 3)]), &mut lab2);
+//       let d: f32 = rgb_d(&lab1[0..3], &lab2[0..3]);
+//       if min > d {
+//         min = d;
+//         id = _i as i32;
+//       }
+//     }
+//     // let js: JsValue = id.into();
+//     // console::log_2(&"Logging :".into(), &js);
+//     output[n] = id as u8;
+//     out_image[4*n    ] = color_map[3*(id as usize)    ] as u8;
+//     out_image[4*n + 1] = color_map[3*(id as usize) + 1] as u8;
+//     out_image[4*n + 2] = color_map[3*(id as usize) + 2] as u8;
+//     out_image[4*n + 3] = 255u8;
+//   }
+// }
+
 #[wasm_bindgen]
-pub fn search_color_id_2(src: &[i32], output: &mut [u8], color_map: &[i32], bg: &[i32], out_image: &mut [u8]) {
+pub fn search_color_id(src: &[i32], output: &mut [u8], color_map: &[i32], bg: &[i32], out_image: &mut [u8], use_method: i32) {
   panic::set_hook(Box::new(console_error_panic_hook::hook));
 
   let mut min:f32 = 100f32;
   let mut id:i32 = 0;
+  let mut lab1: [f32; 4] = [0f32; 4];
+  let mut lab2: [f32; 4] = [0f32; 4];
+  let daynamic_buffer: [[f32; 3]; 16384] = [[0.0; 3]; 16384];
   for n in 0..output.len() {
     id = 100i32;
     min = 1000000f32;
@@ -160,10 +204,65 @@ pub fn search_color_id_2(src: &[i32], output: &mut [u8], color_map: &[i32], bg: 
     ];
     // console::log_5(&src[4*n + 3].into(), &src[4*n    ].into(), &(src[4*n    ] - bg[0]).into(), &((src[4*n    ] - bg[0])*alpha).into(), &((src[4*n    ] - bg[0])*alpha>>8).into());
     for _i in 0..(color_map.len()/3usize) {
-      let d: f32 = ((color_map[3*_i]-a[0]).pow(2)+(color_map[3*_i+1]-a[1]).pow(2)+(color_map[3*_i+2]-a[2]).pow(2)) as f32;
+      rgb2lab(&a, &mut lab1);
+      rgb2lab(&(color_map[3*_i..(3*_i + 3)]), &mut lab2);
+      let mut lab3: [f32; 4] = if use_method & 0b10 == 0b10 {
+        let mut tmp_lab: [f32; 4] = [{
+            let tmp = lab1[0] + daynamic_buffer[n][0];
+            if tmp < 0.0 { 0.0 } 
+            else if tmp > 100.0 { 100.0 }
+            else { tmp }
+          },{
+            let tmp = lab1[1] + daynamic_buffer[n][1];
+            if tmp < -128.0 { -128.0 } 
+            else if tmp > 128.0 { 128.0 }
+            else { tmp }
+          },{
+            let tmp = lab1[2] + daynamic_buffer[n][2];
+            if tmp < -128.0 { -128.0 } 
+            else if tmp > 128.0 { 128.0 }
+            else { tmp }
+          },
+          0.0
+        ];
+        tmp_lab[3] = (tmp_lab[1].powi(2) + tmp_lab[2].powi(2)).sqrt();
+        tmp_lab
+      } else {
+        lab1.clone()
+      };
+      let d :f32 = if use_method & 0b01 == 1 {
+        cie2000(&lab1, &lab2, 1f32, 1f32, 1f32)
+      } else {
+        rgb_d(&lab1[0..3], &lab2[0..3])
+      };
       if min > d {
         min = d;
         id = _i as i32;
+      }
+    }
+    if use_method & 0b10 == 0b10 {
+
+      let _i: usize = id as usize;
+      rgb2lab(&(color_map[3*_i..(3*_i + 3)]), &mut lab2);
+      let err_f: [f32; 3] = [lab1[0] -lab2[0], lab1[1] -lab2[1], lab1[2] -lab2[2]];
+
+      if n%128 < 127 {
+        let mut tmp: &[f32; 3] = &daynamic_buffer[n + 1];
+        tmp = &[tmp[0] + 0.4375*err_f[0], tmp[1] + 0.4375*err_f[1], tmp[2] + 0.4375*err_f[2]];
+      }
+      if n/128 < 127 {
+        if n%128 > 0 {
+          let mut tmp: &[f32; 3] = &daynamic_buffer[n + 127];
+          tmp = &[tmp[0] + 0.1875*err_f[0], tmp[1] + 0.1875*err_f[1], tmp[2] + 0.1875*err_f[2]];
+        }
+        {
+          let mut tmp: &[f32; 3] = &daynamic_buffer[n + 128];
+          tmp = &[tmp[0] + 0.3125*err_f[0], tmp[1] + 0.3125*err_f[1], tmp[2] + 0.3125*err_f[2]];
+        }
+        if n%128 < 127 {
+          let mut tmp: &[f32; 3] = &daynamic_buffer[n + 129];
+          tmp = &[tmp[0] + 0.0625*err_f[0], tmp[1] + 0.0625*err_f[1], tmp[2] + 0.0625*err_f[2]];
+        }
       }
     }
     // let js: JsValue = id.into();
